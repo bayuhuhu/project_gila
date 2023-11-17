0a067c32420943077ebaaad7533ad010a5564fa6
bahyuhuhu <bayuheri2709@gmail.com>
2023-11-17 21:53:32 +0700

    connect to database mongooDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
+mongoose
+.connect(process.env.MONGO)
+.then(()=> {
+    console.log("connected to mongooDB");
+})
+.catch((err) => {
+    console.log(err);
+});
 const app = express();
 
 app.listen(3000, () => {
