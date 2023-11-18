d719d6a62165f541cef0bc3a0a96b8b8fe6b7df2
bahyuhuhu <bayuheri2709@gmail.com>
2023-11-19 01:08:00 +0700

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req, res , next) => {
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashPassword });
@@ -9,7 +11,25 @@ export const signup = async (req, res , next) => {
     await newUser.save();
     res.status(201).json("user telah berhasil ditambahkan");
   } catch (error) {
-    next(error );
+    next(error);
     // next(errorHandler(550, "some issues ")); handle error with costum message
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not Found!!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credential!!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass , ...rest} = validUser._doc;
+    res
+      .cookie('acces_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error)
   }
 };
