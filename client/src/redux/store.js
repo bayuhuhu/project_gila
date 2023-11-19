f134e51a1018e48e122ef843802b8c26e82fa78e
bahyuhuhu <bayuheri2709@gmail.com>
2023-11-19 20:11:25 +0700

    add redux js

---
+++ client/src/redux/store.js
@@ -0,0 +1,10 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import  useReducer  from './user/userSlice';
+
+export const store = configureStore({
+  reducer: { user: useReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
